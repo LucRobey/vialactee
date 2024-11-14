74219ef9e5738875e417ebb24675f74d2fb5e2be
Luc PI <luc.robey@imt-atlantique.net>
2024-11-14 17:49:46 +0100

    Ajout de Listener AUX et de RB_mode simplifié

--- Main.py
+++ Main.py
@@ -1,6 +1,6 @@
 import board
 import neopixel
-pixels1 = neopixel.NeoPixel(board.D18, 2, brightness=1)
+pixels1 = neopixel.NeoPixel(board.D18, 40, brightness=1)
 
 import Listener
 import Modes.Mode as Mode
@@ -8,16 +8,12 @@ import Modes.Rainbow_mode as Rainbow_mode
 import Segment
 
 listener = Listener.Listener()
-segment = Segment.Segment(pixels1)
-rb = Rainbow_mode.Rainbow_mode(listener)
-
+segment1 = Segment.Segment(listener , pixels1)
 
 pixels1.fill((100, 0, 0))
 
 
 
-
-
-
 while True :
-    pass
+    listener.update()
+    segment1.update()
--- Segment.py
+++ Segment.py
@@ -1,9 +1,25 @@
-class Segment:
+import Modes.Mode as Mode
+import Modes.Rainbow_mode as Rainbow_mode
 
-    def __init__(self , leds):
+import numpy as np
+
+class Segment:
+    
+    listener = None
+
+    def __init__(self , listener , leds ):
         self.leds = leds
-        self.modes = None
+        if(self.listener==None):
+            self.listener = listener
+            
+        self.rgb_list=[]
+        for k in range((len(leds))):
+            self.rgb_list.append((0,0,0))
+        self.modes = [Rainbow_mode.Rainbow_mode(self.listener , self.leds , self.rgb_list)]
         self.activ_mode = 0
 
     def update(self):
-        pass
+        self.modes[0].update()
+        
+        for k in range(len(self.rgb_list)):
+            self.leds[k]=self.rgb_list[k]
