67c0f2be521eecc4dea155eb6010ed9e74e1202d
LucRobey <luc.robey@imt-atlantique.net>
2024-12-06 01:57:47 +0100

    ajout de commentaires, sensi,luminosite

--- connectors/Connector.py
+++ connectors/Connector.py
@@ -11,7 +11,7 @@ class Connector:
         self.server_socket.settimeout(0.0001)  # Set timeout to 0.0001 seconds for waiting on connections
 
         self.current_page = "Main"
-        self.list_of_pages = ["Main","Playlists","Configuration","Shot"]
+        self.list_of_pages = ["Main","Playlists","Configuration","Shot","Parametres","Calibration"]
 
         self.list_of_segments = ["Segment h00","Segment v1","Segment h10","Segment h11","Segment v2",
                                  "Segment h20","Segment v3","Segment h30","Segment h31","Segment h32",
@@ -24,24 +24,38 @@ class Connector:
             print("(C) message reçu : ",message)
             splited_message = message.split(":")
             category = splited_message[0]
-            res_of_the_message = message[len(splited_message[0])+1:]
+            rest_of_the_message = message[len(splited_message[0])+1:]
 
             if (category == "chgpage"):
-                order = self.change_page(res_of_the_message)
+                category = splited_message[1]
+                page = splited_message[2]
+                order = self.change_page(category,page)
 
             elif (category == "chgmode"):
-                order = self.change_mode(res_of_the_message)
+                segment = splited_message[1]
+                new_mode = splited_message[2]
+                order = self.change_mode(segment , new_mode)
 
             elif (category == "chgconf"):
-                order = self.change_conf(res_of_the_message)
+                order = self.change_conf(rest_of_the_message)
 
             elif (category == "lockseg"):
                 segment = splited_message[1]
                 lock_unlock = splited_message[2]
                 order = self.block_seg(segment,lock_unlock)
+
+            elif (category == "chgparam"):
+                parametre = splited_message[1]
+                nouvelle_valeur = splited_message[2]
+                order = self.change_param(parametre,nouvelle_valeur)
+
+            elif (category == "calibration"):
+                calibration_type = splited_message[1]
+                phase = splited_message[2]
+                order = self.calibrate_fft(calibration_type,phase)
                 
             elif (category == "special"):
-                order = self.handle_special(res_of_the_message)
+                order = self.handle_special(rest_of_the_message)
                 
             else:
                 print("(C) mauvaise premiere catégorie")
@@ -69,11 +83,8 @@ class Connector:
             #print("No connection received. Doing something else...")
             return None
         
-    def change_page(self , message):
+    def change_page(self , category , page):
         order = []
-        splited_message = message.split(":")
-        category = splited_message[0]
-        page = splited_message[1]
         if (category == "enter"):
             self.current_page = page
             self.check_current_page
@@ -99,21 +110,16 @@ class Connector:
         if (not self.current_page in self.list_of_pages):
             print("(C) CURRENT PAGE PAS DANS LA LISTE")
 
-    def change_mode(self , message):
+    def change_mode(self , segment , new_mode):
         order = []
-        splited_message = message.split(":")
-        segment = splited_message[0]
-        mode = splited_message[1]
-        order.append("change:"+segment+":"+mode)
+        order.append("change:"+segment+":"+new_mode)
         return order
     
     def change_conf(self , message):
         order = []
-
         modes = message[1:-2].split(',')
         for segment_index in range(len(self.list_of_segments)):
             order.append("change:"+self.list_of_segments[segment_index]+":"+modes[segment_index])
-
         return order
     
     def block_seg(self , segment , lock_unlock):
@@ -125,6 +131,27 @@ class Connector:
         else:
             print("(C) ON N EST NI LOCK NI UNLOCK")
         return order
+    
+    def change_param(self , parametre , nouvelle_valeur):
+        order = ["update:"+parametre+":"+str(nouvelle_valeur)]
+        return order
+    
+    def calibrate_fft(self , calibration_type , phase):
+        order = []
+        if (phase == "start"):
+            for segment_name in self.list_of_segments:              # On bloque les segments pdt la calibration
+                order.append("block:"+segment_name)
+            order.append("calibration:"+calibration_type+":"+phase)
+        
+        elif (phase == "end"):
+            for segment_name in self.list_of_segments:
+                order.append("unblock:"+segment_name)               # On les debloque à la fin
+            order.append("calibration:"+calibration_type+":"+phase)
+
+        else:
+            print("(C) ON NE START NI END LA CALIBRATION!")
+        return order
+
     
     def handle_special(self , message):
         return ["special:Shot"]
