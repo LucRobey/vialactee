16815572d5d32d6df2768187a95a1e82fc64afe6
camillefm <camille.francois-martin@imt-atlantique.net>
2026-05-12 14:39:55 +0200

    [update] web app refinement

--- wabb-interface/src/constants/modes.ts
+++
@@ -1,6 +0,0 @@
-export const AVAILABLE_MODES = [
-  "Bary Rainbow", "Chromatic Chaser", "Coloured Middle Wave",
-  "Extending Waves", "Flying Ball", "Hyper Strobe", "Magnetic Ball", "Matrix Rain",
-  "Metronome", "Middle Bar", "Opposite Sides", "PSG", "Plasma Fire", "Power Bar",
-  "Proportion Rainbow", "Rainbow", "Shining Stars", "Static Wave", "Synesthesia"
-];
